caaf2e447b8ac28aa397b0f6b03774de3c2ccf2b
quocanhh86 <160404811+quocanhh86@users.noreply.github.com>
2024-11-22 13:49:37 +0700

    lab5

---
+++ Lab5/src/main/java/com/example/lab5/util/HibernateUtil.java
@@ -0,0 +1,43 @@
+package com.example.lab5.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static final SessionFactory FACTORY;
+
+    static {
+        Configuration conf = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=JoinTable;encrypt=true;trustServerCertificate=true;");
+        properties.put(Environment.USER, "sa");
+        properties.put(Environment.PASS, "123456aA@$");
+        properties.put(Environment.SHOW_SQL, "true"); // gen ra cau sql ma hibernate gen
+
+        conf.setProperties(properties);
+//      conf.addAnnotatedClass(SinhVien1.class); // dang ky hibernate nhan truy van
+//        conf.addAnnotatedClass(NhanVien.class);
+//        conf.addAnnotatedClass(SanPham.class);
+//        conf.addAnnotatedClass(DanhMuc.class);
+        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+
+    }
+
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFACTORY());
+    }
+}
